0687e38e6d31bbb7a9e64823aeef6f1f59906609
Eliasib25 <eliasib.benitez@cecar.edu.co>
2025-06-08 18:25:34 -0500

    interfaz gestión grados corregida

--- views/viewscoordinator/managmentGrade.php
+++ views/viewscoordinator/managmentGrade.php
@@ -3,37 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar grado</title>
+    <title>Gestionar grados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body class="bg-light">
     <div class="container mt-5">
         <div>
             <div class="text-start mt-4">
-                <a href="../dashboard/coordinator.php" class="btn btn-secondary">Volver al dashboard</a>
+                <a href="../dashboard/coordinator.php" class="btn btn-secondary">Volver al dashboard</a> 
+            </div>
+            <h2 class="text-center">Gestión de grados </h2>
+            <p class="text-muted text-center">Aquí puedes gestionar los grados de la institución</p>
+            <div class="mt-4 w-50 mx-auto">
+                <h3>Opciones disponibles:</h3>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="managmentGrade/registerGrade.php" class="text-decoration-none">Registrar grado</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="" class="text-decoration-none">Listado de grados</a> 
+                    </li>
+                </ul>
+            </div>
         </div>
-        <h2 class="text-center">Registrar grado</h2>
-        <p class="text-muted text-center">Aquí puedes registrar nuevos grados para la institución.</p>
-        <form class="w-50 mx-auto" action="../../controllers/RegisterGradeController.php" method="POST">
-            <div class="mb-3">
-                <label for="gradeName" class="form-label">Nombre del grado</label>
-                <input type="text" class="form-control" id="gradeName" name="gradeName" required>
-            </div>
-            <div class="mb-3">
-                <label for="gradeName" class="form-label">Seleccione la jornada</label>
-                     <select class="form-select" id="journey" name="journey" required>
-                        <option value="" disabled selected>Seleccione una jornada</option>
-                        <option value="morning">Mañana</option>
-                        <option value="afternoon">Tarde</option>
-                     </select>
-            </div>
-            <div class="mb-3">
-                <label for="classroom" class="form-label">Aula</label>
-                <input type="text" class="form-control" id="classroom" name="classroom" required>
-            </div>
-            <button type="submit" class="btn btn-primary" name="registerGrade">Registrar grado</button>
-        </form>
-
     </div>
 </body>
 </html>
---
+++ views/viewscoordinator/managmentGrade/registerGrade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar grado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+    <div class="container mt-5">
+        <div>
+            <div class="text-start mt-4">
+                <a href="../managmentGrade.php" class="btn btn-secondary">Regresar</a>
+        </div>
+        <h2 class="text-center">Registrar grado</h2>
+        <p class="text-muted text-center">Aquí puedes registrar nuevos grados para la institución.</p>
+        <form class="w-50 mx-auto" action="../../../controllers/RegisterGradeController.php" method="POST">
+            <div class="mb-3">
+                <label for="gradeName" class="form-label">Nombre del grado</label>
+                <input type="text" class="form-control" id="gradeName" name="gradeName" required>
+            </div>
+            <div class="mb-3">
+                <label for="gradeName" class="form-label">Seleccione la jornada</label>
+                     <select class="form-select" id="journey" name="journey" required>
+                        <option value="" disabled selected>Seleccione una jornada</option>
+                        <option value="morning">Mañana</option>
+                        <option value="afternoon">Tarde</option>
+                     </select>
+            </div>
+            <div class="mb-3">
+                <label for="classroom" class="form-label">Aula</label>
+                <input type="text" class="form-control" id="classroom" name="classroom" required>
+            </div>
+            <button type="submit" class="btn btn-primary" name="registerGrade">Registrar grado</button>
+        </form>
+
+    </div>
+</body>
+</html>
