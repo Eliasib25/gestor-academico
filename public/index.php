139852ae0d92581d0a1304b40bb984a686fbc973
Eliasib25 <eliasib.benitez@cecar.edu.co>
2025-06-11 20:24:33 -0500

    index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php 
 
-    $controller = $_GET['controller'] ?? 'auth';
-    $action = $_GET['action'] ?? 'showLogin';
+    // $controller = $_GET['controller'] ?? 'auth';
+    // $action = $_GET['action'] ?? 'showLogin';
 
-    require_once "../controllers/" . ucfirst($controller) . "Controller.php";
-    $controllerClass = ucfirst($controller) . "Controller";
-    $controllerInstance = new $controllerClass();
-    $controllerInstance->$action();
+    // require_once "../controllers/" . ucfirst($controller) . "Controller.php";
+    // $controllerClass = ucfirst($controller) . "Controller";
+    // $controllerInstance = new $controllerClass();
+    // $controllerInstance->$action();
+
+    header("Location: ../views/auth/login.php");
+    exit();
 
 ?>
